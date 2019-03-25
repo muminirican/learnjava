5ce422e550ee5daf8831bd06123fa1a60f757a39
mumin irican <mumin.irican@siemens.com>
2019-03-25 23:00:20 +0300

    implemented subclasses and go on with inheritence

--- src/com/pluralsight/calcengince/Main.java
+++ src/com/pluralsight/calcengince/Main.java
@@ -33,6 +33,24 @@ public class Main {
         equationOverload.execute(leftDouble, rightDouble);
         System.out.println("result = ");
         System.out.println(equationOverload.getResult());
+
+        System.out.println();
+        System.out.println("using inheritence");
+        System.out.println();
+
+
+        CalculateBase[] calculators = {
+                new Divider(100.0d ,50.0d),
+                new Adder(25.0d ,92.0d ),
+                new Subtracter(225.0d ,17.0d ),
+                new Multiplier(11.0d , 3.0d ), //so this is a comma when inside the block
+        };
+
+        for(CalculateBase a: calculators){
+            a.calculate();
+            System.out.println("result =");
+            System.out.println(a.getResult());
+        }
     }
     // create a new method inside the Main Class
     //public static MathEquation create(double leftVal, double rightVal, char opCode){
